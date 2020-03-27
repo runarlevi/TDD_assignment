ebd6e8ab9bedd8ae80e8f0c591dcb96aa4f0e820
runarlevi <runar18@ru.is>
2020-03-27 11:55:58 +0000

    Step 3: The Add method supports two numbers.

--- calculator.py
+++ calculator.py
@@ -3,4 +3,7 @@ class Calculator(object):
     def Add(input: str) -> int:
         if input == "":
             return 0
+        if "," in input:
+            inputSplitted = input.split(",")
+            return int(inputSplitted[0]) + int(inputSplitted[1])
         return int(input)
--- test_calculator.py
+++ test_calculator.py
@@ -5,3 +5,6 @@ def test_add_empty_returns_zero() -> None:
 
 def test_add_supports_single_number() -> int:
     assert Calculator.Add("9") == 9
+
+def test_add_supports_two_numbers() -> int:
+    assert Calculator.Add("1,2") == 3
